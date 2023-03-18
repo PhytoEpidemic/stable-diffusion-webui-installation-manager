0d71e6aa08bcda3c6fbe7322db3c10a56fce4a32
PhytoEpidemic <64293310+PhytoEpidemic@users.noreply.github.com>
2023-03-18 01:20:16 -0500

    Update install.bat
    removed double quotes

--- extra-installers/ControlNet/install.bat
+++ extra-installers/ControlNet/install.bat
@@ -1,7 +1,11 @@
 @echo off
+
+
+set "tempfolder=%~dp0\sdwebui\%RANDOM%"
+cd %~dp0sdwebui
 call environment.bat
-cd %~dp0
-git clone https://github.com/Mikubill/sd-webui-controlnet.git "%~dp0temp"
-robocopy /E /NJH /NJS /NFL /NDL "%~dp0temp" "%1"
-git -C "%1" reset --hard
-git -C "%1" pull
+git clone https://github.com/Mikubill/sd-webui-controlnet.git "%tempfolder%"
+robocopy /E /NJH /NJS /NFL /NDL "%tempfolder%" %1
+git -C %1 reset --hard
+git -C %1 pull
+rmdir /S /Q "%tempfolder%"
